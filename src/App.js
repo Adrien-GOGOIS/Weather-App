4c072e89e76056f6bd861feb18dee7b095b48f26
Adrien GOGOIS <ad.gogois@gmail.com>
2022-02-23 11:18:51 +0100

    Add 2 main views and install npm packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
 
+// React
+import { createContext, useState } from "react";
+
+// React router dom
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+// Views
+import Home from "./views/Home.jsx";
+import Favorites from "./views/Favorites";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" style={{ margin: "20px" }}>
+                Home
+              </Link>
+              <Link to="/favorites" style={{ margin: "20px" }}>
+                Favorites
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/favorites" component={Favorites} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
