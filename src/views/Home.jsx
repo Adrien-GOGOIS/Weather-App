1b444baf049c6fac0f483bdfb689477aab232c28
Adrien GOGOIS <ad.gogois@gmail.com>
2022-02-23 12:30:00 +0100

    Fetch API weather and display name of the city

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,58 @@
+// Librairies
+import { useForm } from "react-hook-form";
+
+import { useState, useEffect } from "react";
+
 function Home() {
-  return <h1>Home</h1>;
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [city, setCity] = useState("Paris");
+  const [stockage, setStockage] = useState([]);
+
+  const getCity = () => {
+    const value = getValues("location");
+    setCity(value);
+  };
+
+  useEffect(() => {
+    fetch(
+      `api.openweathermap.org/data/2.5/weather?q=Paris&appid=be2cb14537f6eac7f6325a3421aa70e0`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setStockage(res);
+        console.log("res", res);
+      });
+  }, [city]);
+
+  return (
+    <>
+      <div>
+        {/* Fonction soumission formulaire : */}
+        <form onSubmit={handleSubmit(getCity)}>
+          <input
+            {...register("location", { required: true })}
+            type="text"
+            name="location"
+            id="location"
+            placeholder="location"
+            //   value={localStorage.getItem("location")}
+          />
+          {errors.location && <span>Please enter a valid location</span>}
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <div>
+        <p>{city}</p>
+        <p>{stockage.weather}</p>
+      </div>
+    </>
+  );
 }
 
 export default Home;
